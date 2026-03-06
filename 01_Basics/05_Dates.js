85fdeab525087e51bd58675a74e2a70b7204f75e
Rohan Lokhande <rohanlokhande1306@gmail.com>
2026-03-06 19:12:29 +0530

    Updated yt-clone layout

--- 01_Basics/05_Dates.js
+++ 01_Basics/05_Dates.js
@@ -1,3 +1,14 @@
 let myDate = new Date();
-console.log(myDate.toString());
-console.log(myDate.toLocaleDateString);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+let myTime = Date.now();
+console.log(myTime);
+
+let newDate = new Date();
+console.log(newDate.getFullYear());
+console.log(newDate.getDate());
+console.log(newDate.getMinutes());
+console.log(newDate.getHours());
+console.log(newDate.getDay());
