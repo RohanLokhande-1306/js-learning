a76f345619d26cd40c0dd0f4065327ca1d9366e1
Rohan Lokhande <rohanlokhande1306@gmail.com>
2026-03-08 08:49:31 +0530

    Updated yt-clone layout

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,23 @@
+//Array Declearation //
+const myArr = [10,1,2,3,4,5];
+console.log(myArr[0]);
+
+const myName = ["Rohan","Santosh"];
+console.log(myName);
+
+//Array Operations//
+/*
+myName.push("Lokhande");       //Push means Add Data
+console.log(myName);
+myName.push("Hello");
+console.log(myName);
+myName.pop();                 //Pop means remove Data from last 
+console.log(myName);
+myName.unshift("Mr");        //unshifted means add data in first 
+console.log(myName);
+myName.shift();
+console.log(myName);        //Shift means remove first element
+
+console.log(myName.includes("Rohan"));     //Search in Array
+console.log(myName.indexOf("Rohan"));      //Search on index Value
+*/
