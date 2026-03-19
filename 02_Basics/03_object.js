75e06686af2fc865cf3103a6a28f25d4104812a1
Rohan Lokhande <rohanlokhande1306@gmail.com>
2026-03-19 18:59:54 +0530

    Updated yt-clone layout

--- 02_Basics/03_object.js
+++ 02_Basics/03_object.js
@@ -1,7 +1,16 @@
 let Rohan={
-    name :"Shivani",
+    name :"Rohan Lokhande",
     age : 20,
+    location: "Pune",
     email: "rohanlokhande1306@gmail.com"
 };
 
-console.log(Rohan["name"])                 
+Rohan.email = "Rohan@chatgpt.com"  // Replace email value
+Object.freeze(Rohan)
+Rohan.email = "Rohan@microsoft.com"
+
+// console.log(Rohan["name"])
+// console.log(Rohan["location"])
+// console.log(Rohan["email"])
+                  
+console.log(Rohan)
