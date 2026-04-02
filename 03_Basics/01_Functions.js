859d274f4856bd1f2b516848342d24cd4a0e7e4b
Rohan Lokhande <rohanlokhande1306@gmail.com>
2026-04-02 08:04:32 +0530

    Updated yt-clone layout

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -1,4 +1,4 @@
-function MyName()
+function MyName()                // Myname is Function name
 {
 console.log("R")
 console.log("O")
@@ -9,8 +9,27 @@ console.log("N")
 MyName()
 
 
+// parametres passing in function
 function addTwoNumber (name1 ,name2)
 {
     console.log(name1 + name2)
 }
-addTwoNumber("Rohan" ,"Lokhande")
+addTwoNumber("Rohan" ,"Lokhande")     //this Rohan And Lokhande are Parameters
+
+
+
+//if-else in function
+
+function validuswername (username)
+{
+  return "${username}just logg in"
+}
+console.log(validuswername("Rohan"))
+
+
+//rest oprator (...abc)
+function Restoprator (...num1) 
+{
+  return num1
+}
+console.log(Restoprator(200,300,400,500))
